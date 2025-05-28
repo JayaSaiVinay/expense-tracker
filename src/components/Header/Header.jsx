0c2cab3417450ae31a207bfbbc7bc4b7e4cb9978
jayasaivinay <ajsvinay@gmail.com>
2025-05-29 00:20:29 +0530

    project structure and firebase setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import './styles.header.css'
+function Header() {
+  return (
+    <div className='navbar'>Header</div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+import firebaseConfig from "./config.js";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider, setDoc, doc };
